821105495511479e02378dc3a1b65e17e86c849d
robfil62 <45430071+robfil62@users.noreply.github.com>
2019-01-07 17:58:09 +0100

    Modifications du prix (interne et moyenne des echanges)

--- venv/Home.py
+++ venv/Home.py
@@ -1,23 +1,25 @@
-from multiprocessing import Process,Queue,Lock,Value,Event
+from multiprocessing import Process,Queue,Lock,Value,Event,Array
 import time
 import random
 import multiprocessing
 
 nmbHome=5
-nmbTour=3
+nmbTour=5
 delai=4
-def Home(ID,lockQueue,lockCount,queue,count,market_OK,lockWrite,clock_ok):
+starting_price=0.145
+gamma=0.99
+theta=0.01
+def Home(ID,lockQueue,lockCount,queue,count,market_OK,lockWrite,clock_ok,temp,wind,weather_ok):
     Money_Bal = 0
-
     Id = ID
     while clock_ok.wait(1.5*delai): #Attend le top de la clock, et si elle attend plus de 1.5*delai, sort et meurt
 
         flag_Market = False
         neg_Flag = False
 
-        ##Calcul Prod et Cons avec weather
-        Prod = random.randrange(1, 5)   #Production aléatoire
-        Cons = random.randrange(1, 5)   #Consommation aléatoire
+        weather_ok.wait()
+        Prod = random.randrange(1.0, 10.0) * wind[1]  #Production aléatoire
+        Cons = random.randrange(1.0, 10.0) *1/temp[1]  #Consommation aléatoire
         Energy_Bal = Prod - Cons  # Calcul de Energy_Balance pour chaque Home
         with lockWrite:
             print("Energy n°", Id, " : ", Energy_Bal)
@@ -35,6 +37,7 @@ def Home(ID,lockQueue,lockCount,queue,count,market_OK,lockWrite,clock_ok):
         while count.value < nmbHome:  # Attend que toutes les Homes aient rempli la queue avec les demandes
             True
         clock_ok.clear()
+        weather_ok.clear()
 
 
         if Energy_Bal > 0:  # Si la Home est en surplus :
@@ -56,13 +59,13 @@ def Home(ID,lockQueue,lockCount,queue,count,market_OK,lockWrite,clock_ok):
             count.value += 1  # Annonce qu'elle a fini de donner son énergie
 
         while count.value < 2 * nmbHome:  # Attend toutes les Homes
-            True
+            pass
 
         if Energy_Bal < 0: # Si la Home est en déficit, regarde si quelqu'un à répondu à sa demande :
             Tab=[] #Crée un tableau où elle va mettre toutes les demandes qui ne la concerne pas
             with lockQueue:
                 while not queue.empty() or queue.qsize()!=0 : # Tant que la queue n'est pas vide
-                    demande = queue.get() #Récupère une demande
+                    demande = queue.get()#Récupère une demande
                     if demande[0] == Id:  # Regarde si la demande nous concerne
                         Energy_Bal = demande[1]  # Si oui, met à jour son Energy_Bal
                         neg_Flag = True
@@ -80,6 +83,7 @@ def Home(ID,lockQueue,lockCount,queue,count,market_OK,lockWrite,clock_ok):
         if Energy_Bal != 0:     #Une fois toutes les demandes traitées, si elles ont encore besoin d'énergies ou peuvent encore en donner
             with lockQueue:
                 queue.put([Id, Energy_Bal]) #Place leur énergie dans la queue à destination du market
+                print(Energy_Bal)
                 flag_Market = True  #Si elles ont placées quelque chose pour le market, elles iront chercher la réponse de celui-ci
             Energy_Bal = 0  #Elle achète ou vend son énergie pour être à 0
 
@@ -115,24 +119,36 @@ def Home(ID,lockQueue,lockCount,queue,count,market_OK,lockWrite,clock_ok):
 #############Wait clock################################
 
 
-
-
-
-
-
-def Market(queue,count,market_OK,clock_ok):
+def Market(queue,count,market_OK,clock_ok,temp,wind):
+    currentPrice=starting_price
+    moy_exchange=0.0
+    Exchange=0.0
     while clock_ok.wait(1.5*delai):
-        currentPrice=10
         market_OK.value=False
+        internal=temp[0]*temp[1]+wind[0]*wind[1]-theta*moy_exchange
+        external=0
+        currentPrice=gamma*currentPrice+internal+external
 
         while count.value<3*nmbHome:
             True
         size=queue.qsize()
+        tab=[]
+
         for i in range(0,size):
             demande=queue.get()
-            queue.put([demande[0],demande[1]*currentPrice])
+            print("demande",demande[1])
+            tab.append(demande)
 
+        for d in tab :
+            queue.put([d[0],d[1]*currentPrice])
+
+        print(currentPrice,moy_exchange,Exchange)
         market_OK.value=True
+        Exchange=0.0
+        for i in tab:
+            Exchange+=i[1]
+
+        moy_exchange=Exchange/len(tab)
 
 def Clock(clock_ok,):
     c=0
@@ -145,32 +161,60 @@ def Clock(clock_ok,):
         print("**********************************")
 
 
+def Weather(temp,wind,clock_ok,weather_ok):
+    temp[0]=0.001
+    wind[0]=0.001
+    while clock_ok.wait(1.5*delai):
+        temp[1]=round(random.gauss(10.0,15.0),2)
+        wind[1]=round(random.gauss(20.0,15.0),2)
+
+        print("TEMP:",temp[1],"wind:",wind[1])
+        temp[1]=(temp[1]+6.0)/18.0
+        wind[1]=(wind[1])/20.0
+        print(temp[1],wind[1])
+        weather_ok.set()
+        time.sleep(delai)
+
 if __name__=="__main__":
     ti=time.time()
     lockQueue = Lock()
     lockCount=Lock()
     lockWrite=Lock()
+
     queue=Queue()
-    count=Value('i',0)
+
     clock_ok=Event()
-    Homes=[]
+    weather_ok=Event()
+
+    count=Value('i',0)
+    temp=Array('f',range(2))
+    wind=Array('f',range(2))
     market_OK=Value('b',False)
-    c = Process(target=Clock, args=(clock_ok, ))
-    c.start()
+
+    Homes=[]
+
+
 
     for i in range(1,nmbHome+1):
-        h=Process(target=Home, args=(i,lockQueue,lockCount,queue,count,market_OK,lockWrite,clock_ok),)
+        h=Process(target=Home, args=(i,lockQueue,lockCount,queue,count,market_OK,lockWrite,clock_ok,temp,wind,weather_ok),)
         h.start()
         Homes.append(h)
-    m=Process(target=Market, args=(queue,count,market_OK,clock_ok))
+
+    m=Process(target=Market, args=(queue,count,market_OK,clock_ok,temp,wind))
     m.start()
+
+    w=Process(target=Weather,args=(temp,wind,clock_ok,weather_ok))
+    w.start()
+
+    c = Process(target=Clock, args=(clock_ok,))
+    c.start()
 
     c.join()
     m.join()
+    w.join()
+
     for h in Homes:
         h.join()
-
-
 
     print("temps de la simulation:",time.time()-ti)
 
